1f238f68bb2c6866beed6de49344be675a2c6106
unknown <BT_3W2_26@BKTECH.LOCAL>
2022-10-18 09:29:47 -0400

    Initial commit

---
+++ src/LinearEquation.java
@@ -0,0 +1,14 @@
+public class LinearEquation {
+
+    int xCoordinate1;
+    int yCoordinate2;
+
+
+    public int SlopeCalculator(xCoordinate1, yCoordinate2) {
+        double finalSlope =
+        return finalSlope;
+
+    }
+
+    if(finalSlope *  xCoordi
+}
---
+++ src/LinearEquationRunner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class LinearEquationRunner {
+    public static void main(String []args){
+        System.out.println("Welcome!");
+        System.out.println("Please enter in a coordinate pair in this form: (x, y)");
+        System.out.println("Please enter in another coordinate pair in this form: (x, y)");
+
+
+
+    }
+}
